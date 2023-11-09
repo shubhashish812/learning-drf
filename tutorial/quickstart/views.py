44cfe9ee84e59ba9e2f3e62b5fc45c7d349c0a24
shubhashish812 <125235973+shubhashish812@users.noreply.github.com>
2023-11-08 21:52:23 -0800

    Adding Tutorial Project
    1. Added Quickstart App
    2. Implemented Serializers.py

---
+++ tutorial/quickstart/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    # Endpoint - User Views or edits
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    
+
